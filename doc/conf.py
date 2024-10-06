993e0cba6e42fba6ca004bd094f02585acfee86d
Brandi McPherson <brandimcpher@gmail.com>
2024-10-06 12:48:36 -0400

    updated conf

--- doc/conf.py
+++ doc/conf.py
@@ -27,6 +27,6 @@ html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 html_logo = "freeflyer.png"
 html_theme_options = {
-    'logo_only': True,
+    'logo_only': False,
     'display_version': False,
 }
